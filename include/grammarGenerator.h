d5205d503a0edbaa7b9312ae5127a3f1b971908c
Eric K Richardson <ekrichardson@gmail.com>
2025-10-09 17:20:05 -0700

    Update docs and Doxygen file (#18)
    * Use entities to fix embedded XML in HTML docs in tests/check_exip.c
    
    * Resolve Doxygen document warnings
    
    * Upgrade Doxyfile, unknown a4wide to default a4

--- include/grammarGenerator.h
+++ include/grammarGenerator.h
@@ -39,12 +39,12 @@ typedef enum SchemaFormat SchemaFormat;
 
 /**
  * @brief Call-back handler, to be implemented by the applications
- * It is called to resolve references to schemas (<include> and <import>)
+ * It is called to resolve references to schemas (&lt;include&gt; and &lt;import&gt;)
  * not included in the BinaryBuffer list of generateSchemaInformedGrammars().
  *
- * @param[in] namespace NULL for <include> reference; the target namespace of <import>-ed schema otherwise
- * @param[in] schemaLocation can be NULL for <import> reference only; The uri location of the schema otherwise
- * @param[out] buffers an array of input buffers holding the representation of the schema <include>-ed or <import>-ed
+ * @param[in] namespace NULL for &lt;include&gt; reference; the target namespace of &lt;import&gt;-ed schema otherwise
+ * @param[in] schemaLocation can be NULL for &lt;import&gt; reference only; The uri location of the schema otherwise
+ * @param[out] buffers an array of input buffers holding the representation of the schema &lt;include&gt;-ed or &lt;import&gt;-ed
  *             Each buffer refers to one schema file.
  * @param[out] bufCount the number of buffers in the array
  * @param[out] schemaFormat EXI, XSD, DTD or any other schema representation supported
@@ -53,8 +53,8 @@ typedef enum SchemaFormat SchemaFormat;
  * options defined in the EXI header of the XSD stream then this parameter must be NULL.
  * @return Error handling code
  *
- * @note In the case of <include> - both the host- and included-schema must have identical target namespace.
- * That is, the current implementation does not support the case of using <include> from XML Schema
+ * @note In the case of &lt;include&gt; - both the host- and included-schema must have identical target namespace.
+ * That is, the current implementation does not support the case of using &lt;include&gt; from XML Schema
  * that has non empty target namespace and the referenced/included XML schema have an empty target
  * namespace, although this is allowed by the XML Schema spec.
  */
@@ -73,12 +73,12 @@ errorCode loadSchemaHandler(String* namespace, String* schemaLocation, BinaryBuf
  * for processing the EXI XSD stream although no options are specified in the EXI header. If there are
  * options defined in the EXI header of the XSD stream then this parameter must be NULL.
  * @param[out] schema the resulted schema information used for processing EXI streams
- * @param[in] loadSchemaHandler Call-back handler for loading <include>-ed or <import>-ed schema files; Can be left NULL
- * if no <include> or <import> statements are used in the XML schema.
+ * @param[in] loadSchemaHandler Call-back handler for loading &lt;include&gt;-ed or &lt;import&gt;-ed schema files; Can be left NULL
+ * if no &lt;include&gt; or &lt;import&gt; statements are used in the XML schema.
  * @return Error handling code
  *
- * @note In the case of <include> - both the host- and included-schema must have identical target namespace.
- * That is, the current implementation does not support the case of using <include> from XML Schema
+ * @note In the case of &lt;include&gt; - both the host- and included-schema must have identical target namespace.
+ * That is, the current implementation does not support the case of using &lt;include&gt; from XML Schema
  * that has non empty target namespace and the referenced/included XML schema have an empty target
  * namespace, although this is allowed by the XML Schema spec.
  */
