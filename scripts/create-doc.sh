3c5c73a230f62ad43eb9c69005c627e74cf85c04
Eric K Richardson <ekrichardson@gmail.com>
2026-02-16 18:24:37 -0800

    Update script and image location

---
+++ scripts/create-doc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Create the user documentation PDF
+set -eux
+
+# Change the the source directory
+cd "$(dirname "$0")/../docs/user"
+
+# Create the PDF and move into the www directory
+tectonic -X compile exip.tex  && mv exip.pdf ../www/exip-user-guide-1.pdf
