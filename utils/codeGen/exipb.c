8feb666a048978f4dee1ef18604c6f84fd337604
Eric K Richardson <ekrichardson@gmail.com>
2026-05-18 10:18:39 -0700

    Start a schema binding generator, minor rename to schemaGen

---
+++ utils/codeGen/exipb.c
@@ -0,0 +1,21 @@
+/*==================================================================*\
+|                EXIP - Embeddable EXI Processor in C                |
+|--------------------------------------------------------------------|
+|          This work is licensed under Apache License 2.0            |
+|  The full license terms and conditions are located in LICENSE.txt  |
+|            Copyright (C) 2024 - 2026, ekrich.org                   |
+\===================================================================*/
+
+/**
+ * @file exipb.c
+ * @brief Command-line utility for generating C bindings from EXI schemas
+ * @date May 18, 2026
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Code Gen\n");
+    return 0;
+}
